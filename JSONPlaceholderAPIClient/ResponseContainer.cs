07e489bd2b847f524859698c9a874c64d026b3d2
Yuliia Mykhailenko <Yuliia_Mykhailenko@epam.com>
2018-01-24 18:58:28 +0200

    added new class

--- JSONPlaceholderAPIClient/ResponseContainer.cs
+++ JSONPlaceholderAPIClient/ResponseContainer.cs
@@ -1,9 +1,14 @@
-﻿namespace JSONPlaceholderAPIClient
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace JSONPlaceholderAPIClient
 {
     public class ResponseContainer<T>
     {
-        //DeserealizedContent
-
-        //    StatusCode
+        public CreatePostModel DeserealizedContent { get; set; }
+        public CreatePostModel StatusCode { get; set; }
     }
 }
